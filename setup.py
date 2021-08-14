2917eb406220bd6798da7f09fb2ab6b6c3a56a5b
Cyberbolt <735245473@qq.com>
2021-08-14 23:46:00 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clock_timer",
-    version="0.1.0",
+    version="0.1.1",
     author="Cyberbolt",
     author_email="735245473@qq.com",
     description="clock_timer 是 Python 下的时间库，用于时间字符串处理，可在 Web 开发、数据分析 等领域使用。该库 80% 基于 datetime ，使用该库，您能更人性化地处理时间字符串，而无需每次查询 datetime 繁琐的接口。",
